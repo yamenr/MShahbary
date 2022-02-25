9ab3163c9075ac21ddc4b146d8e8ec2437f0b9f2
Mohammad <tests2pdp@gmail.com>
2022-02-25 10:14:24 +0200

    Fixed main activity_main.xml i) linked a button to the method "GotoSignUp" in MainActivity.java -Filled the MainGame.java with code from the previous project -Designed the activity_main_game.xml
    - added Slash class and code in GameView"
    - added some notes next to the codes in GameView
    -fixed some xml designs [18/2/22]
    -More xml designs and gameview fixes
    -Removed MainGame Activity [25/2/22]

--- app/src/main/java/com/example/tfotmauthtest/GameActivity.java
+++ app/src/main/java/com/example/tfotmauthtest/GameActivity.java
@@ -15,22 +15,22 @@ public class GameActivity extends AppCompatActivity {
         getSupportActionBar().hide();
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-    Point point = new Point();
-    getWindowManager().getDefaultDisplay().getSize(point);
+    Point point = new Point();// to get screen size
+    getWindowManager().getDefaultDisplay().getSize(point);// this will pass our point object and now this point object contains the size of our screen on X-axis and Y-axis
 
-    gameView = new GameView(this, point.x, point.y);
+    gameView = new GameView(this, point.x, point.y); // this is to get our size of the screen on Xand Y axis
 
-    setContentView(gameView);
+    setContentView(gameView); // this is to show our gameview
 }
 
     @Override
-    protected void onPause() {
+    protected void onPause() { // so when gameview pauses this also pauses
         super.onPause();
         gameView.pause();
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume() { // when gameview resumes this also resumes
         super.onResume();
         gameView.resume();}
 }
